80f833a177fbe3cfd0c5b07bd3a726435cfae1c8
jrucld <ysk789465@gmail.com>
2021-10-05 22:17:47 +0900

    Issue #1 - 프로젝트 initial 세팅 추가

---
+++ src/main/java/com/detectivecat/shiftwork/ShiftworkApplication.java
@@ -0,0 +1,13 @@
+package com.detectivecat.shiftwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/detectivecat/shiftwork/ShiftworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.detectivecat.shiftwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
